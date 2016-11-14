9dd2fa3e7aeea845e78df33c1380d80f41e982a0
kreyesdr <kreyes616@gmail.com>
2016-11-14 10:08:39 -0400

    Done with events controllers

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,11 @@ Rails.application.routes.draw do
   #   root to: 'devise/registrations#new'
   # end
 
+  namespace :api, defaults: { format: :json } do
+    match '/events', to: 'events#preflight', via: [:options]
+    resources :events, only: [:create]
+  end
+
   root 'main#index'
   resources :applications
 
